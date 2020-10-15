b4e7753e696724928fa21581d0b67af8fc4f4592
我要买可乐 <2448548211@qq.com>
2020-10-15 14:07:00 +0800

    第一次提交项目

---
+++ src/com/qf/chen/app/Demo01.java
@@ -0,0 +1,13 @@
+package com.qf.chen.app;
+
+/**
+ * @author ChenWang
+ * @className Demo01
+ * @date 2020/10/15 14:00
+ * @since JDK 1.8
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
